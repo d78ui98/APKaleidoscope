8cfb1a226ed8f464188fde0685d997b55804cf47
d78ui98 <gajbhiye.deepanshu@gmail.com>
2023-08-16 08:58:37 -0400

    added a section in output to list down dangerous permission

--- APKaleidoscope.py
+++ APKaleidoscope.py
@@ -151,6 +151,48 @@ class AutoApkScanner(object):
 
         indent = "    "
 
+        DANGEROUS_TYPES = [
+            "android.permission.READ_CALENDAR",
+            "android.permission.WRITE_CALENDAR",
+            "android.permission.CAMERA",
+            "android.permission.READ_CONTACTS",
+            "android.permission.WRITE_CONTACTS",
+            "android.permission.GET_ACCOUNTS",
+            "android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.ACCESS_COARSE_LOCATION",
+            "android.permission.RECORD_AUDIO",
+            "android.permission.READ_PHONE_STATE",
+            "android.permission.READ_PHONE_NUMBERS",
+            "android.permission.CALL_PHONE",
+            "android.permission.ANSWER_PHONE_CALLS",
+            "android.permission.READ_CALL_LOG",
+            "android.permission.WRITE_CALL_LOG",
+            "android.permission.ADD_VOICEMAIL",
+            "android.permission.USE_SIP",
+            "android.permission.PROCESS_OUTGOING_CALLS",
+            "android.permission.BODY_SENSORS",
+            "android.permission.SEND_SMS",
+            "android.permission.RECEIVE_SMS",
+            "android.permission.READ_SMS",
+            "android.permission.RECEIVE_WAP_PUSH",
+            "android.permission.RECEIVE_MMS",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.MOUNT_UNMOUNT_FILESYSTEMS",
+            "android.permission.READ_HISTORY_BOOKMARKS",
+            "android.permission.WRITE_HISTORY_BOOKMARKS",
+            "android.permission.INSTALL_PACKAGES",
+            "android.permission.RECEIVE_BOOT_COMPLETED",
+            "android.permission.READ_LOGS",
+            "android.permission.CHANGE_WIFI_STATE",
+            "android.permission.DISABLE_KEYGUARD",
+            "android.permission.GET_TASKS",
+            "android.permission.BLUETOOTH",
+            "android.permission.CHANGE_NETWORK_STATE",
+            "android.permission.ACCESS_WIFI_STATE",
+        ]
+        dangerous_permissions = [perm for perm in data['permissions'] if perm in DANGEROUS_TYPES]
+
         util.mod_log(f"[+] Package Name:", util.OKCYAN)
         print(indent + data['package_name'] + "\n")
 
@@ -163,6 +205,12 @@ class AutoApkScanner(object):
         if data['permissions']:
             util.mod_log(f"[+] Permissions:", util.OKCYAN)
             for permission in data['permissions']:
+                print(indent + permission)
+            print()
+        
+        if dangerous_permissions:
+            util.mod_log(f"[+] Dangerous Permissions:", util.FAIL)
+            for permission in dangerous_permissions:
                 print(indent + permission)
             print()
 
